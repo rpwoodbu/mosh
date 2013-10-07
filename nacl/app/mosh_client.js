1f06f64fbd29e6fe0c370b5cf5d2bb470144f04f
Richard Woodbury <rpwoodbu@mybox.org>
2013-10-07 18:18:10 -0400

    Cleanup of NaCl embed creation and terminal display style.

--- nacl/app/mosh_client.js
+++ nacl/app/mosh_client.js
@@ -15,44 +15,11 @@
 
 'use strict';
 
-var MoshClientModule = null;  // Global application object.
-var statusText = 'NO-STATUS';
-
-// Indicate load success.
-function moduleDidLoad() {
-  updateStatus('SUCCESS');
-}
-
-// The 'message' event handler.  This handler is fired when the NaCl module
-// posts a message to the browser by calling PPB_Messaging.PostMessage()
-// (in C) or pp::Instance.PostMessage() (in C++).  This implementation
-// simply displays the content of the message in an alert panel.
-function handleMessage(message_event) {
-  updateStatus(message_event.data);
-}
-
-// If the page loads before the Native Client module loads, then set the
-// status message indicating that the module is still loading.  Otherwise,
-// do not change the status message.
 window.onload = function() {
-  var listener = document.getElementById('listener');
-  listener.addEventListener('load', moduleDidLoad, true);
-
-  if (MoshClientModule == null) {
-    updateStatus('LOADING...');
-  } else {
-    // It's possible that the Native Client module onload event fired
-    // before the page's onload event.  In this case, the status message
-    // will reflect 'SUCCESS', but won't be displayed.  This call will
-    // display the current message.
-    updateStatus();
-  }
-
   function execMosh() {
     var terminal = new hterm.Terminal("mosh");
     terminal.decorate(document.querySelector('#terminal'));
     terminal.onTerminalReady = function() {
-      updateStatus('Terminal ready');
       terminal.setCursorPosition(0, 0);
       terminal.setCursorVisible(true);
       terminal.runCommandClass(mosh.CommandInstance, '');
@@ -63,16 +30,6 @@ window.onload = function() {
   };
 
   lib.init(execMosh, console.log.bind(console));
-}
-
-// Set the global status message.  If the element with id 'statusField'
-// exists, then set its HTML to the status message as well.
-// opt_message The message test.  If this is null or undefined, then
-// attempt to set the element with id 'statusField' to the value of
-// |statusText|.
-function updateStatus(opt_message) {
-  if (opt_message)
-    statusText = opt_message;
 }
 
 var mosh = {};
@@ -96,13 +53,30 @@ mosh.CommandInstance.prototype.run = function() {
   // Useful for console debugging.
   window.mosh_client_ = this;
 
-  this.moshNaCl_ = document.querySelector("#mosh_client");
-  this.moshNaCl_.addEventListener('message', this.onMessage_.bind(this));
-
   this.io = this.argv_.io.push();
-
-  this.io.println("It works!");
   this.io.onVTKeystroke = this.onKeystroke_.bind(this);
+
+  this.moshNaCl_ = window.document.createElement('embed');
+  this.moshNaCl_.style.cssText = (
+      'position: absolute;' +
+      'top: -99px' +
+      'width: 0;' +
+      'height: 0;');
+  this.moshNaCl_.setAttribute('src', 'mosh_client.nmf');
+  this.moshNaCl_.setAttribute('type', 'application/x-nacl');
+  this.moshNaCl_.setAttribute('key', 'ExmWlnI90NCuZCSMobsGSg');
+  this.moshNaCl_.setAttribute('addr', '192.168.11.103');
+  this.moshNaCl_.setAttribute('port', '60001');
+  this.moshNaCl_.addEventListener('load', function(e) {
+    console.log('Mosh NaCl loaded.');
+  });
+  this.moshNaCl_.addEventListener('message', this.onMessage_.bind(this));
+  this.moshNaCl_.addEventListener('crash', function(e) {
+    console.log('Mosh NaCl crashed.');
+    self.exit(-1);
+  });
+
+  document.body.insertBefore(this.moshNaCl_, document.body.firstChild);
 };
 
 mosh.CommandInstance.prototype.onMessage_ = function(e) {
