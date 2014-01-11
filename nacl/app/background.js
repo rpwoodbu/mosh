b2990e8799a17155ea169f759b67711645859cbf
Richard Woodbury <rpwoodbu@mybox.org>
2014-01-11 17:03:31 -0500

    Add window ID to allow persistence of position and size.

--- nacl/app/background.js
+++ nacl/app/background.js
@@ -1,8 +1,5 @@
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('mosh_client.html', {
-    'bounds': {
-      'width': 800,
-      'height': 600
-    }
+    'id': 'mosh_client',
   });
 });
